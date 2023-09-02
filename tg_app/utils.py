e02df0b195783782cc7bd61a227bcd420c1ca825
nu-denis <nu.denis15@gmail.com>
2023-09-02 21:50:01 +0300

    skip None message

--- tg_app/utils.py
+++ tg_app/utils.py
@@ -2,6 +2,8 @@ def get_last_message_id_by_chat_id(last_updates):
     res = {}
     for upd in last_updates:
         print('upd.message\n\n', upd.message)
+        if not upd.message:
+            continue
         chat_info = upd.message.chat
         chat_id = chat_info.id
         message_id = upd.message.id
